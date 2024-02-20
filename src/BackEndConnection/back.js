61e90f382a0406f1739b46b5a13845fce9e95c7a
Carlaurence <64654800+Carlaurence@users.noreply.github.com>
2024-02-20 00:25:33 -0500

    Update back.js

--- src/BackEndConnection/back.js
+++ src/BackEndConnection/back.js
@@ -1,6 +1,6 @@
 /***CONEXION DESDE EL FRONT AL PUERTO DE SALIDA DEL BACKEND***/
 module.exports = {
     api:{
-        baseURL: process.env.REACT_APP_API
+        baseURL: process.env.REACT_APP_BACKEND_URL
     }
 }
